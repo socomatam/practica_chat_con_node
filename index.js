d50f4fc3583d577e9c6fe913fbc22efa821a1c2d
Carlos <satizabal44@gmail.com>
2020-02-19 19:23:18 +0000

    Se puede enviar mensaje privados

--- index.js
+++ index.js
@@ -7,22 +7,21 @@ app.get('/', function(req, res) {
 });
 
 var usuarios = [];
-
+var socketsId = [];
 
 //abre conexión
 io.on('connection', function(socket) {
 	
 	//recibe el nombre de usuario al que se le enviará un privado
+	//también recibe el socket.id literal 
 	socket.on('mensaje_privado', function(usuario){
-		console.log(usuario.id);
-		console.log(usuario.usuario);
-		io.to(usuario.id).emit('privado', 'Este mensaje es único')
+		socketsId.push(usuario.id);
 	});//fin mensaje privado
 	
 	
 	//recibe los mensaje de los usuarios y los envía
 	socket.on('chat message', function(msg) {
-		//divide la frase resibida en el mensaje de un usuario
+		//divide la frase recibida en el mensaje de un usuario
 		var divisiones = msg.mensaje.split(" ");
 		
 		var mensaje = "";
@@ -43,11 +42,9 @@ io.on('connection', function(socket) {
 			mensaje = mensaje + divisiones[i];
 			mensaje = mensaje + " ";
 		}//fin for
-		 console.log(msg.color);
+
 		
-		
-		//io.emit('chat message', mensaje);
-		
+		//envía a los clientes los mensajes de todos
 		io.emit('chat message', {
 			mensaje:mensaje,
 			usuario:msg.usuario,
@@ -57,10 +54,10 @@ io.on('connection', function(socket) {
 	
 	socket.on('usuario_activo', function(usuario) {
 		//io.emit('usuario_activo', usuario);
+		
 		io.emit('usuario_activo', {usuario: usuario, id: socket.id});
 	});//fin disconect
 
-	
 	socket.on('disconnect', function() {
 		//emite si algún usuario ha salido del chat
 		io.emit('usuario_desconectado', socket.username);
@@ -76,16 +73,31 @@ io.on('connection', function(socket) {
 		io.emit('usuario_noescribiendo', usuario);
 	});//fin nuevo usuario
 	
-
 	//recibe el nombre de usuario
-	socket.on('nuevo_usuario', function(usuario) {
-	
+	socket.on('nuevo_usuario', function(usuario) {	
 		socket.username = usuario;
 		//emite que el usuario nuevo se ha creado
-		
 		//io.emit('usuario_activo', {usuario: usuario, id: socket.id});
 		io.emit('usuario_conectado', usuario);
 	});//fin nuevo usuario
+	
+	socket.on('borrar_seleccion_usuarios', function(orden){
+		socketsId = [];
+		console.log('Id de usuarios borrador');
+	});////fin borrar
+	
+	//envía el mensaje privado
+	socket.on('privado',function(mensaje){
+
+		for(var i = 0; i < socketsId.length; i++){
+			//console.log(socketsId[i]);
+			io.to(socketsId[i]).emit('privado', mensaje);
+		}//fin for
+
+		//io.to().emit('privado', 'Este mensaje es único');
+	});//fin privado
+	
+
 }); //fin conection
 
 http.listen(80, function() {
